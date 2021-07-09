c58c8195129469f9e09c79a5cf9f0eec11557330
Andi <allushiandi13@gmail.com>
2021-07-09 14:09:02 +0200

    Aggiornato corretto

--- src/main/java/org/example/web/AppController.java
+++ src/main/java/org/example/web/AppController.java
@@ -1,47 +1,30 @@
 package org.example.web;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping; // uso getmapping
 import org.springframework.web.bind.annotation.RestController;
+
+import org.example.App;
 
 @RestController
 public class AppController {
 
+    App app = new App();
+
     @GetMapping("/fattoriale/{numero}")
     public  int Calcolofattoriale(@PathVariable("numero") int numero){
-        int fatt = 1;
-
-        if (numero == 0) {
-            return 1;
-        } else {
-            for (int i = 1; i <= numero; i++) {
-                fatt = fatt * i;
-            }
-        }
-        return fatt;
+        return app.fattoriale(numero);
     }
 
     @GetMapping("/potenza/{a}/{b}")
-    public int potenza(@PathVariable("a") int a,@PathVariable("b")int b){
-        int pot = 1;
-        if(b==0){
-            return 1;
-        }
-        else{
-
-            for(int k=b;k>0;k--){
-                pot = pot*a;
-            }
-        }
-        return pot;
+    public int Elevamentopotenza(@PathVariable("a") int a,@PathVariable("b")int b){
+       return app.potenza(a,b);
     }
 
     @GetMapping("/coeffbinomiale/{n}/{k}")
-    public  int binomiale(@PathVariable("n") int n,@PathVariable("k")int k){
-        int n_fatt = Calcolofattoriale(n);
-        int k_fatt = Calcolofattoriale(k);
-        int n_k_fatt = Calcolofattoriale(n-k);
-        return n_fatt/(k_fatt*n_k_fatt);
+    public  int Coefficientebinomiale(@PathVariable("n") int n,@PathVariable("k")int k){
+        return app.binomiale(n,k);
     }
 
 
