c11255a690d1b0c1c0150e3181efc25f97798ba9
Andi <allushiandi13@gmail.com>
2021-07-13 16:57:46 +0200

    Caso sensato di mockito

---
+++ src/test/java/org/example/TestController.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.example.web.AppController;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+
+
+
+public class TestController {
+    @Test
+    public void firstTest(){
+        App mockedList = mock(App.class); // creo mock della classe App ( che rappresenta il mio servizio con i metodi )
+        //given
+        when(mockedList.fattoriale(4)).thenReturn(24);
+        //when
+        AppController appController = new AppController(mockedList);
+        int numero = appController.Calcolofattoriale(4);
+        //then
+        verify(mockedList,times(1)).fattoriale(4);
+
+        //invece scrivendo:         verify(mockedList,times(2)).fattoriale(4);
+        //ottengo errore perchè non ho invocato 2 volte il metodo, ma 1 volta
+    }
+    //Quindi il senso è che io avendo il @Service ho dei metodi, che possono anche non essere implementati, dunque utilizzando
+    //mockito posso far si che io simuli il comportamento di alcuni metodi e continuare il test.
+    //
+}
