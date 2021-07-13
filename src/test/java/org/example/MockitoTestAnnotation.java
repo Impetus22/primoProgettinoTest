dc7cab854f5fabb47286392e7d5dba7960383cb8
Andi <allushiandi13@gmail.com>
2021-07-13 15:51:03 +0200

    a

---
+++ src/test/java/org/example/MockitoTestAnnotation.java
@@ -0,0 +1,37 @@
+package org.example;
+import static org.mockito.Mockito.*;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+//aggiungo questa annotazione
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoTestAnnotation {
+
+    @Mock
+    App mockedList;
+
+    @Test
+    public void binomialeMockitoTest(){
+        mockedList.binomiale(5,2);
+        verify(mockedList).binomiale(5,2);
+        when(mockedList.binomiale(3,3)).thenReturn(1);
+        Assertions.assertEquals(mockedList.binomiale(3,3),1);
+
+    }
+    @Test
+    public void coeffBinMockitoTest(){
+        when(mockedList.fattoriale(5)).thenReturn(120);
+        when(mockedList.fattoriale(4)).thenReturn(24);
+        when(mockedList.fattoriale(1)).thenReturn(1);
+        mockedList.binomiale(5,4);
+    }
+
+
+
+}
---
+++ src/test/java/org/example/MockitoTestingStatic.java
@@ -0,0 +1,58 @@
+package org.example;
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+
+
+import static org.mockito.Mockito.*;
+
+/*BREVE TEORIA
+
+1)si creano dei mock in due modi, in modo statico usando mock() oppure tramite annotation @Mock
+2)Principalmente si crea un mock di una classe perchè magari istanziarla non è comodo, oppure posono esserci
+metodi che non sono facili da implementare
+quindi creando un mock e usando when() e thenReturn() io simulo già il funzionamento del metodo e posso usarlo per continuare il mio test
+guarda esempio con assertEquals()
+3)dato che il mock registra tutte le interazioni, è molto utile per verificare quante volte un metodo viene chiamato
+guarda l'esempio introduttivo
+*/
+public class MockitoTestingStatic {
+
+            @Test
+            public void firstMockitoTest(){
+                App mockedList = mock(App.class); // STATIC MOCK della classe App da testare
+                //NB. i metodi della classe mock-ata non devono essere static/private!!
+                when(mockedList.fattoriale(3)).thenReturn(6);
+                System.out.println(mockedList.fattoriale(3)); // questo sa farlo perchè l'ho detto sopra
+                // System.out.println(mockedList.fattoriale(2)); // questo no
+
+                //per questa riga, metto il language 8 nel modulo del progetto
+                //doAnswer(invocation -> "inserire numero positivo").when(mockedList).fattoriale(-3);
+
+
+            }
+
+            @Test
+            public void secondMockitoTest(){
+                App mockedList = mock(App.class);
+                //avvio il metodo
+                mockedList.binomiale(5,2);
+                //verifico che il metodo sia partito
+                //verify(mockedList).binomiale(5,2); // esempio che funziona
+                // verify(mockedList).binomiale(3,2); //esempio che non funziona
+                // verify(mockedList).fattoriale(5); // esempio di errore dove però mi mostra una interazione con .binomiale(5,2);
+                // se ora aggiungo il seguente codice :
+                // mockedList.binomiale(7,5);
+                // facendo rieseguire
+                // verify(mockedList).fattoriale(5); // mi dice che ho 2 interazioni
+            }
+            @Test
+            public void invocationsMockitoTest(){
+                App mockedList = mock(App.class);
+// invoco 5 volte (?)
+                verify(mockedList, times(5));
+
+            }
+}
