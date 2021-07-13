817a8d4a9884f6d4daadb8613c7f69f3c1e6caa5
Andi <allushiandi13@gmail.com>
2021-07-13 17:41:52 +0200

    ** caso sensato di mockito **

--- src/test/java/org/example/TestController.java
+++ src/test/java/org/example/TestController.java
@@ -1,9 +1,9 @@
 package org.example;
+
 import org.example.web.AppController;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
-
 
 
 public class TestController {
